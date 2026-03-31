3eb456d5b314782c72cf4ab978b22fa0a5c972f4
Kristina <studenikinacz@gmail.com>
2026-03-31 18:40:08 +0200

    Finish number system conversion

--- main.js
+++ main.js
@@ -4,18 +4,53 @@
 /**
  * TODO - Write functional code for this application. You can call any other function, but usage of ".toString(numberSystem)" and "Number.parseInt(number, numberSystem)" is forbidden (only permitted when used on individual digits).
  * The main function which calls the application. 
- * TODO - Please, add specific description here for the application purpose.
+ * Converts a number from one base to another.
+ * First converts the input number to decimal, then from decimal to the target base.
  * @param {string} inputNumber number that is being converted
  * @param {number} inputNumberSystem numerical system that the inputNumber is being converted from
  * @param {number} outputNumberSystem numerical system that the inputNumber is being converted into
  * @returns {string} containing number converted to output system
  */
 export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
-  //TODO code
-  //let dtoOut = exMain(inputNumber, inputNumberSystem, outputNumberSystem);
+  if (inputNumberSystem === outputNumberSystem) {
+    throw new Error("Input and output number systems must be different.");
+  }
+  
+  let dtoOut = exMain(inputNumber, inputNumberSystem, outputNumberSystem);
   return dtoOut;
 }
 
+export function exMain(inputNumber, inputNumberSystem, outputNumberSystem) {
+  let decimal = toDecimal(inputNumber, inputNumberSystem);
+  return fromDecimal(decimal, outputNumberSystem);
+}
+
+function toDecimal(inputNumber, inputNumberSystem) {
+  let result = 0;
+  let power = 0;
+
+  for (let i = inputNumber.length - 1; i >= 0; i--) {
+    let digit = Number(inputNumber[i]);
+    result += digit * Math.pow(inputNumberSystem, power);
+    power++;
+  }
+
+  return result;
+}
+
+function fromDecimal(number, base) {
+  if (number === 0) return "0";
+
+  let result = "";
+
+  while (number > 0) {
+    let remainder = number % base;
+    result = remainder + result;
+    number = Math.floor(number / base);
+  }
+
+  return result;
+}
 /**
  * TODO - Change this to contain all input number systems that your application can convert from.
  * Function which returns which number systems are permitted on input.
